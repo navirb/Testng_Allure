4ce4647f5da35cb289a7172c467cce370fb47441
navirb <91819150+navirb@users.noreply.github.com>
2023-09-03 18:43:16 +0530

    Update Base.java

--- src/test/java/com/tutorialsninja/qa/base/Base.java
+++ src/test/java/com/tutorialsninja/qa/base/Base.java
@@ -65,7 +65,7 @@ public class Base {
 			System.out.println("==================================");
 			//System.out.println(System.getProperty("user.dir"));
 			//WebDriverManager.chromedriver().setup();
-			//System.setProperty("webdriver.chrome.driver","C:\\Users\\pratm\\.cache\\selenium\\chromedriver\\win64\\116.0.5845.96\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\pratm\\.cache\\selenium\\chromedriver\\win64\\116.0.5845.96\\chromedriver.exe");
 			System.out.println("==================================");
 			options.setBinary("C:\\Users\\pratm\\.cache\\selenium\\chromedriver\\win64\\116.0.5845.96\\chromedriver.exe");
 
